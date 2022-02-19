abe6ccab061e6d5bf4e00e4cc8d26def536830fc
chinmay1819 <chinmaydeshpande34@gmail.com>
2022-02-19 21:33:05 +0530

    commit

--- DSA/linkedlist/singly.cpp
+++ DSA/linkedlist/singly.cpp
@@ -1,10 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Node{
+    public:
+    int data;
+    Node*next;
+
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+
+
+ 
+
+
+};
+void print(Node*head){
+     Node*p=head;
+     if(p==NULL)
+        return;
+     cout<<p->data<<endl;
+     print(p->next);   
+ }
+
 
 
 
 
 int main(){
-
+    Node*head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    head->next->next->next=new Node(40);
+    head->next->next->next->next=new Node(50);
+    print(head);
 }
