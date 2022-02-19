e99aa670da04dec2b2acbce5b1b73853d4bf6e8f
chinmay1819 <chinmaydeshpande34@gmail.com>
2022-02-20 00:10:30 +0530

    commit

--- DSA/linkedlist/doubly.cpp
+++ DSA/linkedlist/doubly.cpp
@@ -12,13 +12,66 @@ class Node{
         next=prev=NULL;
     }
 };
+Node*push(Node*head,Node*temp){
+    if(head==NULL)
+        return head;
+
+    head->next=temp;
+    temp->prev=head;
+    return head;
+    
+}
+
+
+int pop(Node*head){
+    Node*p=head;
+    if(p==NULL)
+        return -1;
+    while (p->next!=NULL)
+    {
+        p=p->next;
+    }
+    int res=p->data;
+    p->prev->next=NULL;
+    
+    return res;
+    
+
+}
+
+
+
+
+
+
+
+
+void print(Node*head){
+    Node*p=head;
+    if(p==NULL)
+        return ;
+
+    cout<<p->data<<endl;
+    print(p->next);
+
+}
+
+
 
 int main(){
     Node*head=new Node(10);
-    head->next=new Node(20);
-    head->prev=NULL;
-    head->next->prev=head;
-    head->next->next=new Node(30);
+    Node*temp=new Node(20);
+    push(head,temp);
+    // print(head);
+    Node*temp2=new Node(30);
+    Node*temp3=new Node(40);
+    push(temp,temp2);
+    push(temp2,temp3);
+
+    cout<<pop(head)<<endl;
     
+    print(head);
+
     
+   
 }
